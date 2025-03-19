c250e4123bce0abdc8cbae67454871d58e732d3a
Gatien Delhotal <gdelhota@c2r3p6.42perpignan.fr>
2025-03-19 17:09:05 +0100

    moved useful functions from utils to libft

--- libft/libft.h
+++ libft/libft.h
@@ -6,7 +6,7 @@
 /*   By: gdelhota <gdelhota@student.42perpignan.fr> +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/05 15:11:11 by gdelhota          #+#    #+#             */
-/*   Updated: 2025/03/19 15:17:12 by gdelhota         ###   ########.fr       */
+/*   Updated: 2025/03/19 17:02:00 by gdelhota         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,6 +27,7 @@ typedef struct s_dll
 int		min(int a, int b);
 int		max(int a, int b);
 int		abs(int n);
+void	ft_error(char *error);
 int		ft_safe_atoi(const char *str, int *res);
 t_dll	*ft_lstnew(int content);
 void	ft_lstdelone(t_dll *lst);
@@ -34,6 +35,7 @@ void	ft_lstclear(t_dll *lst);
 t_dll	*ft_lstpop(t_dll **lst);
 t_dll	*ft_lststack(t_dll *node, t_dll *stack);
 t_dll	*ft_lstpush(t_dll **node, t_dll *stack);
+int		is_sorted_dll(t_dll *lst);
 int		get_dll_size(t_dll *lst);
 
 #endif
