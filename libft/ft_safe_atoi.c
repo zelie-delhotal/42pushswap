3a8f4073ed16a8d00e594f7f332b088740d4d65d
zelie-delhotal <gatien.delhotal@orange.fr>
2025-03-25 00:33:57 +0100

    fixed errors on sign parsing

--- libft/ft_safe_atoi.c
+++ libft/ft_safe_atoi.c
@@ -6,16 +6,19 @@
 /*   By: gdelhota <gdelhota@student.42perpigna      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/30 16:42:42 by gdelhota          #+#    #+#             */
-/*   Updated: 2025/01/30 19:00:11 by gdelhota         ###   ########.fr       */
+/*   Updated: 2025/02/05 18:11:32 by gdelhota         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+// converts *str to int and stores the value in res
+// sets res to 0 and returns false if *str is not a number or has 2+ signs
 int		ft_safe_atoi(const char *str, int *res)
 {
 	int	i;
 	int	sign;
 
 	*res = 0;
+	sign = 1;
 	i = 0;
 	if (str[i] == '+' || str[i] == '-')
 	{
@@ -30,3 +33,19 @@ int		ft_safe_atoi(const char *str, int *res)
 	}
 	return (str[i] == 0);
 }
+
+/*#include <stdio.h>
+int main (int ac, char **av)
+{
+	int	i;
+	int	value;
+
+	i = 0;
+	while (++i < ac)
+	{
+		if (ft_safe_atoi(av[i], &value))
+			printf("%d\n", value);
+		else
+			printf("error\n");
+	}
+}*/
